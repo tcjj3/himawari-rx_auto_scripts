f7371fd5c43c71c47aaf29077a66eb72ad04674a
Chaojun Tan <495825947@qq.com>
2020-12-29 11:01:13 +0800

    Add files via upload

---
+++ Windows/start.bat
@@ -0,0 +1,25 @@
+@echo off
+title himawari-rx_auto_scripts_starter
+
+
+
+
+
+echo '-----------------------------------------------------------'
+echo '                        start.bat                          '
+echo '                This is the starter script.                '
+echo '                                                           '
+echo '   @tcjj3    github.com/tcjj3/himawari-rx_auto_scripts     '
+echo '-----------------------------------------------------------'
+
+
+
+
+
+start himawari-rx__auto.bat
+
+start time_monitor_to_terminate_TSDuck.bat
+
+
+
+
